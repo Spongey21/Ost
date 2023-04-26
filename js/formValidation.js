a3cfb3ac465a15e01a0d01dae15475e161e86db5
Spongey21 <driton.latifi101@gmail.com>
2023-04-26 13:55:52 +0200

    .

--- js/formValidation.js
+++ js/formValidation.js
@@ -3,16 +3,34 @@ const VALIDATION = (function() {
         let errorCount = 0;
 
         for (let i = 0; i < form.children.length - 1; i++) {
-            if (form.children[i].children[1].value.length == 0) {
-                errors[i].textContent = `Please fill out: ${form.children[i].children[0].textContent}`
+            let element = form.children[i];
+
+            if (element.children[1].value.length == 0) {
+                errors[i].textContent = `Please fill out: ${element.children[0].textContent}`
                 errorCount++;
+            } else if (element.children[1].name == 'phone' && element.children[1].value.length == 8) {
+                let validNumber = /^\+?[1-9][0-9]{7,14}$/
+
+                if (!validNumber.test(element.children[1].value)) {
+                    errors[i].textContent = `Please fix: ${element.children[0].textContent}`
+                    errorCount++;
+                }
+            } else if (element.children[1].name == 'email') {
+                let validEmailRegex = /^\S+@\S+\.\S+$/
+
+                if (!validEmailRegex.test(element.children[1].value)) {
+                    errors[i].textContent = `Please fix: ${element.children[0].textContent}`
+                    errorCount++;
+                }
             } else {
-                errors[i].textContent = ''
+                element.children[1].value = ''
+                errors[i].textContent = `Please fill out: ${element.children[0].textContent}`
+                errorCount++;
             }
         }
 
         if (errorCount == 0) {
-            FORM.submit();
+            form.submit();
         }
     }
 
